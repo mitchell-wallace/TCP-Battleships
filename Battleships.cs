2ab3a95eec12e6222beda3962eb7f57dd4f7e473
Mitch <38238516+mitchell-wallace@users.noreply.github.com>
2022-10-09 13:10:19 +1100

    init

---
+++ Battleships.cs
@@ -0,0 +1,20 @@
+
+
+namespace Battleships {
+
+    public class Battleships {
+
+        public static void Main() {
+
+            Console.WriteLine("Battleships!!");
+
+        }
+
+
+
+
+    }
+
+
+
+}
